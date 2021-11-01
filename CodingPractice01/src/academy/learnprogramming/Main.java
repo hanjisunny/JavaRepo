6caf7c67e9d3383f20ba90df792e0a4041bad6fb
hanjisunny <jisun0405@skku.edu>
2021-11-01 14:00:30 +0900

    coding practice 1

---
+++ CodingPractice01/src/academy/learnprogramming/Main.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args){
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour/1.609344);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+
+}
